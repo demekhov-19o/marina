80ced6cb55412a47806f57a93b4cfe9bf7e0c24e
Oleg <demekhov@yandex.ru>
2022-09-21 06:55:52 +0300

    марина

--- itog/Program.cs
+++ itog/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 string[] array1 = new string[5] {"Я", "люблю", "hellooooo", "woooorld", "Марину"};
 string[] array2 = new string[array1.Length];

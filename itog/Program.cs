79931f588759efc1642b2ee2e3e0e6e445792438
Oleg <demekhov@yandex.ru>
2022-09-21 06:25:05 +0300

    люблю

--- itog/Program.cs
+++ itog/Program.cs
@@ -4,3 +4,15 @@ Console.WriteLine("Hello, World!");
 string[] array1 = new string[5] {"Я", "люблю", "hellooooo", "woooorld", "Марину"};
 string[] array2 = new string[array1.Length];
 
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 6)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
